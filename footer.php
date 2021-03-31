b314c7a2fe9caf7f160c0cea59a03f47d3979c05
nickular <nrchat@gmail.com>
2021-03-31 11:00:09 -0400

    consolidate footer
    Info, logo, etc now in one place instead of split for logged in vs not logged in

--- footer.php
+++ footer.php
@@ -1,9 +1,19 @@
 <?php
 
 if (isset($_SESSION['username']) && $_SESSION['username'] == "notrack")  {
-    include "footer_nologin.php";
+    // include "footer_nologin.php";
 } else  {
     include "footer_login.php";
 }
+
 ?>
 
+<hr width=50%><center>
+<table border=0 width=600><tr><td align="center"><a href="/centaur/"><img alt="Centaur" src="logo.png"></A><BR><font size=-2>Build 03/22/2021</font>
+<TD>
+<font size=-1>Hospital of University of Pennsylvania<BR>
+Department of Radiology <BR>
+Neil Chatterjee.  Original code by Po-Hao (Howard) Chen <BR><a href="https://www.gnu.org/copyleft/gpl.html">GNU Public License</A>
+</font>
+</center>
+</tr></table>
--- footer_login.php
+++ footer_login.php
@@ -57,15 +57,6 @@ if (isset($_SESSION['username'])) {
 
 END;
 ?>
-<hr width=50%><center>
-<table border=0 width=600><tr><td align="center"><a href="/centaur/"><img alt="Centaur" src="logo.png"></A><BR><font size=-2>Build 03/22/2021</font>
-<TD>
-<font size=-1>Hospital of University of Pennsylvania<BR>
-Department of Radiology <BR>
-Neil Chatterjee.  Original code by Po-Hao (Howard) Chen <BR><a href="https://www.gnu.org/copyleft/gpl.html">GNU Public License</A>
-</font>
-</center>
-</tr></table>
 
 </body>
 </html>
